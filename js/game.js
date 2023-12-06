9aa4d63bbd141e77b6bd60f35efc09b1a9fa2259
Haris Chughtai <xharisahmed98@gmail.com>
2023-12-06 17:07:57 +0000

    finished

--- js/game.js
+++ js/game.js
@@ -22,6 +22,8 @@ class Game {
 
     this.setupRestartButton();
     this.setupMusic();
+
+    this.setupRestartButton();
   }
 
   startGame() {
@@ -45,7 +47,6 @@ class Game {
         }
       }, i * 550);
     }
-
     this.playBackgroundMusic();
   }
 
@@ -114,6 +115,7 @@ class Game {
         this.gameScreen.removeChild(obstacle.element);
       }
     });
+
     this.obstacles = null;
     this.gameScreen.style.display = "none";
 
@@ -156,5 +158,70 @@ class Game {
   stopBackgroundMusic() {
     this.backgroundMusic.pause();
     this.backgroundMusic.currentTime = 0;
+  }
+
+  activateEasterEgg() {
+    cancelAnimationFrame(this.animateId);
+    this.gameScreen.style.display = "none";
+    this.easterEgg.style.display = "flex";
+    this.easterEgg.style.flexDirection = "column";
+    this.stopBackgroundMusic();
+
+    const easterEggImage = document.createElement("img");
+    easterEggImage.src = "Game_assets/mat.jpg";
+    easterEggImage.style.width = "100%";
+    easterEggImage.style.height = "100%";
+
+    const easterEggText = document.createElement("div");
+    easterEggText.innerText =
+      "I can simply snap my fingers and it'll all cease to exist";
+    easterEggText.style.position = "absolute";
+    easterEggText.style.top = "90%";
+    easterEggText.style.left = "50%";
+    easterEggText.style.transform = "translate(-50%, -50%)";
+    easterEggText.style.color = "rgba(255, 255, 255)";
+    easterEggText.style.fontSize = "4em";
+    easterEggText.style.textAlign = "center";
+    easterEggText.style.fontFamily = "Impact, Charcoal, sans-serif";
+    easterEggText.style.letterSpacing = "4px";
+    easterEggText.style.width = "100%";
+    easterEggText.style.textTransform = "uppercase";
+
+    const backButton = document.createElement("button");
+    backButton.innerText = "Home";
+    backButton.id = "back-btn";
+    this.setStyle(backButton);
+
+    backButton.addEventListener("click", () => {
+      location.reload();
+    });
+
+    this.easterEgg.innerHTML = "";
+    this.easterEgg.appendChild(easterEggImage);
+    this.easterEgg.appendChild(easterEggText);
+    this.easterEgg.appendChild(backButton);
+
+    void easterEggImage.offsetWidth;
+
+    let opacity = 0;
+    const fadeInInterval = setInterval(() => {
+      opacity += 0.01;
+      easterEggImage.style.opacity = opacity;
+      if (opacity >= 1) {
+        clearInterval(fadeInInterval);
+      }
+    }, 20);
+  }
+
+  setStyle(element) {
+    element.position = "absolute";
+    element.style.width = "30%";
+    element.style.padding = "20px";
+    element.style.boxSizing = "border-box";
+    element.style.color = "rgba(255, 255, 255)";
+    element.style.fontSize = "2em";
+    element.style.fontFamily = "Impact, Charcoal, sans-serif";
+    element.style.letterSpacing = "2px";
+    element.style.textTransform = "uppercase";
   }
 }
